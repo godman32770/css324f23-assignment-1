84290ec62edc44e9449275b9666b80b14f30bcc5
godman32770 <144578657+godman32770@users.noreply.github.com>
2023-09-11 21:37:05 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,36 +1,41 @@
+def initial_state():
+    return (8, 0, 0)
+
 def is_goal(s):
-    x, y, z = s
-    return x == 4 and y == 4
+    return s[0] == 4 and s[1] == 4
 
 def successors(s):
-    x, y, z = s
-    successor_states = []
+    actions = []
+    a, b, c = s
 
-    # Define the pour operation from one bottle to another
-    def pour(source, destination):
-        nonlocal x, y, z
-        state_list = list(s)  # Convert the tuple to a list for modification
-        if state_list[source] > 0 and state_list[destination] < destination_capacity[destination]:
-            amount_to_pour = min(state_list[source], destination_capacity[destination] - state_list[destination])
-            state_list[source] -= amount_to_pour
-            state_list[destination] += amount_to_pour
-            successor_states.append(tuple(state_list))  # Convert the list back to a tuple
+    # Pour from the 8-liter bottle to the 5-liter bottle
+    if a > 0 and b < 5:
+        amount = min(a, 5 - b)
+        actions.append(((a - amount, b + amount, c), amount))
 
-    # Define the capacities of the bottles
-    destination_capacity = [8, 5, 3]
+    # Pour from the 8-liter bottle to the 3-liter bottle
+    if a > 0 and c < 3:
+        amount = min(a, 3 - c)
+        actions.append(((a - amount, b, c + amount), amount))
 
-    # Generate all possible pour operations
-    pour(0, 1)  # Pour from the 8-liter bottle to the 5-liter bottle
-    pour(0, 2)  # Pour from the 8-liter bottle to the 3-liter bottle
-    pour(1, 0)  # Pour from the 5-liter bottle to the 8-liter bottle
-    pour(1, 2)  # Pour from the 5-liter bottle to the 3-liter bottle
-    pour(2, 0)  # Pour from the 3-liter bottle to the 8-liter bottle
-    pour(2, 1)  # Pour from the 3-liter bottle to the 5-liter bottle
+    # Pour from the 5-liter bottle to the 8-liter bottle
+    if b > 0 and a < 8:
+        amount = min(b, 8 - a)
+        actions.append(((a + amount, b - amount, c), amount))
 
-    return successor_states
+    # Pour from the 5-liter bottle to the 3-liter bottle
+    if b > 0 and c < 3:
+        amount = min(b, 3 - c)
+        actions.append(((a, b - amount, c + amount), amount))
 
-# Test the functions
-initial = initial_state()
-print("Initial state:", initial)
-print("Is goal state:", is_goal(initial))
-print("Successor states:", successors(initial))
+    # Pour from the 3-liter bottle to the 8-liter bottle
+    if c > 0 and a < 8:
+        amount = min(c, 8 - a)
+        actions.append(((a + amount, b, c - amount), amount))
+
+    # Pour from the 3-liter bottle to the 5-liter bottle
+    if c > 0 and b < 5:
+        amount = min(c, 5 - b)
+        actions.append(((a, b + amount, c - amount), amount))
+
+    return actions  
